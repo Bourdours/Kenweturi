f8b611d7b3e6694545c7b0d73a4d90dc2f74ad2d
ROCA <rcamenen@gmail.com>
2026-04-29 14:37:59 +0200

    base CodeIgniter4

---
+++ app/Controllers/Home.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controllers;
+
+class Home extends BaseController
+{
+    public function index(): string
+    {
+        return view('welcome_message');
+    }
+
+    public function test(int $id, int $id2): string
+    {
+        return view('test',["id1"=>$id,"id2"=>$id2]);
+    }
+}
