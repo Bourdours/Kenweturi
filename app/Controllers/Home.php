033361108406f7208a6bd6e774ec58aefe8929a7
Bruno <brunomiguel1997dh@gmail.com>
2026-04-30 10:40:57 +0200

    feat: ajout des vues partielles (head, header, footer)

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,7 +6,7 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        return view('welcome_message');
+        return view('home');
     }
 
     public function test(int $id, int $id2): string
