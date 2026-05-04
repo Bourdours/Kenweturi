dcce629b827bf95bbf5768d7e9d1cd45e675cd06
Bruno <brunomiguel1997dh@gmail.com>
2026-05-04 13:23:32 +0200

    feat: ajouter le header et la page d'inscription

--- app/Views/partials/header.php
+++ app/Views/partials/header.php
@@ -1,6 +1,21 @@
 <body>
 <header>
-    <nav>
-        
-    </nav>
+
+  <div>
+    <div>
+      <i class="fa-solid fa-car-side"></i>
+    </div>
+    <span>Kenweturi</span>
+  </div>
+
+  <nav>
+    <a href="#">Trouver un trajet</a>
+    <a href="#">Proposer un trajet</a>
+    <a href="#">Comment ça marche</a>
+  </nav>
+
+  <div>
+    <a href="#">Connexion</a>
+    <a href="<?= site_url('inscription') ?>" >Inscription</a>
+  </div>
 </header>
