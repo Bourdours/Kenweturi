6183d5c25ba64da0a31062881c13b0b7faae0e61
Bruno <brunomiguel1997dh@gmail.com>
2026-05-05 10:12:07 +0200

    feat(auth): ajouter les fonctionnalités de connexion et déconnexion

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -13,4 +13,11 @@ $routes->get('/inscription', 'AuthController::register');
 $routes->get('/login', 'AuthController::login');
 $routes->post('/inscription', 'AuthController::handleRegister');
 $routes->post('handleRegister', 'AuthController::handleRegister');
+
+// Route pour afficher la page (déjà existante normalement)
+$routes->get('login', 'AuthController::login');
+
+// Route pour TRAITER le formulaire de connexion
+$routes->post('login/check', 'AuthController::handleLogin');
+
 $routes->get('/test/(:num)/test/(:num)', 'Home::test/$1/$2');
--- app/Views/home.php
+++ app/Views/home.php
@@ -1,6 +1,13 @@
 <?= view('partials/head') ?>
 <?= view('partials/header') ?>
 
+    <!-- Message de succès après inscription (test) -->
+    <?php if (session()->getFlashdata('success')): ?>
+        <div>
+            <?= session()->getFlashdata('success') ?>
+        </div>
+    <?php endif; ?>
+
 <main>
     <h1>Accueil</h1>
 </main>
