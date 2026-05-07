5ed160a807909f8351ce06f4e3e3e0cab93dc8a4
Tllnc <ninotollance@gmail.com>
2026-05-07 16:07:03 +0200

    fix: corriger un conflit entre développeurs

--- app/Models/CityModel.php
+++ app/Models/CityModel.php
@@ -9,26 +9,26 @@ class CityModel extends BaseModel
 {
     protected $table = 'city';
     protected $primaryKey = 'id';
-    protected $userAutoIncrement = true;
+    protected $useAutoIncrement = true;
     
-    protected $allowFields = [
+    protected $allowedFields = [
         'name',
         'zipcode'
     ];
 
     protected $validationRules = [
-        'name' => 'required|min_length[2]|max_length[50]',
+        'name'    => 'required|min_length[2]|max_length[50]',
         'zipcode' => 'required|exact_length[5]|numeric'
     ];
 
     protected $validationMessages = [
         'name' => [
-            'required' => 'Le nom de la ville est obligatoire.',
+            'required'   => 'Le nom de la ville est obligatoire.',
             'min_length' => 'Le nom doit contenir au moins 2 caractères.',
         ],
         'zipcode' => [
             'required'   => 'Le code postal est obligatoire.',
-            'max_length' => 'Le code postal doit contenir 5 caractères.',
+            'max_length' => 'Le code postal doit contenir exactement 5 chiffres.',
             'numeric'    => 'Le code postal ne doit contenir que des chiffres.',
         ],
     ];
