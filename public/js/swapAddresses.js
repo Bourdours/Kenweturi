3d782fde9823dac97de6a8c73b36a62337d93405
Bourdours <maillot.gaetan@yahoo.fr>
2026-05-27 14:52:36 +0200

    ajout bouton swap + js

---
+++ public/js/swapAddresses.js
@@ -0,0 +1,12 @@
+document.getElementById('swapAddresses').addEventListener('click', function () {
+    const pairs = [
+        ['startAddress', 'endAddress'],
+        ['startAddressLng', 'endAddressLng'],
+        ['startAddressLat', 'endAddressLat'],
+    ];
+    pairs.forEach(([a, b]) => {
+        const elA = document.getElementById(a);
+        const elB = document.getElementById(b);
+        [elA.value, elB.value] = [elB.value, elA.value];
+    });
+});
